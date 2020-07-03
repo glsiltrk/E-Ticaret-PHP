f2ab914ec122f79910bc23f426388ac64c7342ba
melahatakin <56938288+melahatakin@users.noreply.github.com>
2020-07-03 05:30:59 +0300

    Update baglan.php

--- eticaret/baglan.php
+++ eticaret/baglan.php
@@ -1,9 +1,13 @@
 <?php
 try {
      $db = new PDO("mysql:host=localhost;dbname=eticaret", "root", "");
-     echo"BASARILI";
+     //echo"BASARILI";
 } catch ( PDOException $e ){
      print $e->getMessage();
      exit;
 }
+
+$Ayar-$db->query("SELECT * FROM ayar")->fetch();
+
+print_r($Ayar);
 ?>
