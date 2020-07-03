4c367f1d524e41c740839bab41ad34f432ea63c4
melahatakin <56938288+melahatakin@users.noreply.github.com>
2020-07-03 07:57:18 +0300

    Update ayar_guncelle.php
    değerleri göndermek için bir dizi oluşturdum.
    dizinin elemanlarını belirtirken 'ayar_baslik' => bu dizinin indisinin ismi oldu şimdi ise bu indis elemanın değerini verdik.$POST['ayar_baslik'] bir indise yaz

--- eticaret/yonet/ayar_guncelle.php
+++ eticaret/yonet/ayar_guncelle.php
@@ -16,6 +16,23 @@
   		ayar_mport:=ayar_mport,
   		ayar_mkadi:=ayar_mkadi,
   		ayar_msifre:=ayar_msifre
-  		WHERE ayar_id=1")
+  		WHERE ayar_id=1");
+  	$guncelle->execute(
+array(
+     'ayar_baslik'=>$_POST['ayar_baslik'],
+     'ayar_description'=>$_POST['ayar_description'],
+     'ayar_keywords'=>$_POST['ayar_keywords'],
+     'ayar_facebook'=>$_POST['ayar_facebook'],
+     'ayar_twitter'=>$_POST['ayar_twitter'],
+     'ayar_instagram'=>$_POST['ayar_instagram'],
+     'ayar_youtube'=>$_POST['ayar_youtube'],
+     'ayar_msunucu'=>$_POST['ayar_msunucu'], 
+     'ayar_mport'=>$_POST['ayar_mport'],
+     'ayar_mkadi'=>$_POST['ayar_mkadi'],
+     'ayar_msifre'=>$_POST['ayar_msifre'],
+       
+
+)
+  	);
   }
   ?>
